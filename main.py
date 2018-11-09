fca25925dc143669b5195b0ddaa0a30947f7b561
mbeckering <mikebeckering@gmail.com>
2018-11-09 13:50:09 -0600

    Add victory dance
    Added victory dance function. Added function call at the end of movement.

--- main.py
+++ main.py
@@ -2,6 +2,13 @@
 import cozmo
 from cozmo.util import degrees, distance_inches, speed_mmps
 
+########## Speech & Animation Functions ##########
+def victoryDance(robot):
+    robot.play_anim_trigger(cozmo.anim.Triggers.CodeLabVictory).wait_for_completed()
+    robot.turn_in_place(degrees(180)).wait_for_completed()
+    robot.play_anim_trigger(cozmo.anim.Triggers.DanceMambo).wait_for_completed() # this dance is kinda long
+    robot.say_text("Yay for computer science!").wait_for_completed()
+
 ########## Utility Functions ##########
 def driveInches(robot, inches):
     robot.drive_straight(distance_inches(inches), speed_mmps(50)).wait_for_completed()
@@ -22,6 +29,7 @@ def main(robot: cozmo.robot.Robot):
     driveInches(robot, 10)
     turnLeft(robot)
     driveInches(robot, 10)
+    victoryDance(robot)
 
 ########## Run Program ##########
 cozmo.run_program(main)
