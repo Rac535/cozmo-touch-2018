0ef92eb29a2504248bb427119330ca31079a7581
mbeckering <mikebeckering@gmail.com>
2018-11-14 14:47:17 -0600

    Added "random" (but hard-coded) routing through maze.
    I have removed the two modular squares from the test maze, and Cozmo can now use the whole test maze with this iteration of main.py.
    
    main() function now rolls (1-4) different routes through the TEST MAZE, random each run.
    
    Cozmo now speaks before turning around if he hits a dead end.
    
    To increase testing efficiency:
    Increased speed of forward motion within the driveInches() function. Commented out the victoryDance() function call at the end (temporarily).

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import cozmo
+import random
 from cozmo.util import degrees, distance_inches, speed_mmps
 
 ########## Speech & Animation Functions ##########
@@ -11,7 +12,7 @@ def victoryDance(robot):
 
 ########## Utility Functions ##########
 def driveInches(robot, inches):
-    robot.drive_straight(distance_inches(inches), speed_mmps(50)).wait_for_completed()
+    robot.drive_straight(distance_inches(inches), speed_mmps(80)).wait_for_completed()
 
 def turnDegrees(robot, nDegrees):
     robot.turn_in_place(degrees(nDegrees)).wait_for_completed()
@@ -22,14 +23,60 @@ def turnRight(robot):
 def turnLeft(robot):
     turnDegrees(robot, 90)
 
+'''
+This iteration of the TEST MAZE main function will choose a random path through the TEST MAZE before beginning
+movement of Cozmo.
+'''
 ########## Main ##########
 def main(robot: cozmo.robot.Robot):
-    driveInches(robot, 9)
-    turnRight(robot)
-    driveInches(robot, 10)
-    turnLeft(robot)
-    driveInches(robot, 10)
-    victoryDance(robot)
+    roll = random.randint(1, 4)
+
+    # PATH 1: Shortest path through TEST MAZE
+    if roll == 1:
+        driveInches(robot, 9)  # Start Line to First "T"
+        turnRight(robot)
+        driveInches(robot, 9.95)  # First "T" to Second "T"
+        turnLeft(robot)
+        driveInches(robot, 10)  # Second "T" to Finish Line
+
+    # PATH 2: Hit Dead End 1, then proceed to end of TEST MAZE
+    elif roll == 2:
+        driveInches(robot, 13.95)  # Start Line to Dead End 1
+        robot.say_text("Dead end!").wait_for_completed()
+        robot.turn_in_place(degrees(180)).wait_for_completed()
+        driveInches(robot, 5)  # Dead End 1 back to First "T"
+        turnLeft(robot)
+        driveInches(robot, 9.95)  # First "T" to Second "T"
+        turnLeft(robot)
+        driveInches(robot, 10)  # Second "T" to Finish Line
+
+    # PATH 3: Hit both dead ends before proceeding to end of TEST MAZE
+    elif roll == 3:
+        driveInches(robot, 13.95)  # Start Line to Dead End 1
+        robot.say_text("Dead end!").wait_for_completed()
+        robot.turn_in_place(degrees(180)).wait_for_completed()
+        driveInches(robot, 5)  # Dead End 1 back to First "T"
+        turnLeft(robot)
+        driveInches(robot, 9.95)  # First "T" to Second "T"
+        turnRight(robot)
+        driveInches(robot, 4.7)  # Second "T" to Dead End 2
+        robot.say_text("Another dead end!").wait_for_completed()
+        robot.turn_in_place(degrees(178)).wait_for_completed()
+        driveInches(robot, 14.2)  # Dead End 2 to Finish Line
+
+    # PATH 4: Right at first "T", right into Dead End 2, then proceed to end of TEST MAZE
+    elif roll == 4:
+        driveInches(robot, 9)  # Start Line to First "T"
+        turnRight(robot)
+        driveInches(robot, 9.95)  # First "T" to Second "T"
+        turnRight(robot)
+        driveInches(robot, 4.7)  # Second "T" to Dead End 2
+        robot.say_text("Dead end!").wait_for_completed()
+        robot.turn_in_place(degrees(178)).wait_for_completed()
+        driveInches(robot, 14.2)  # Second "T" to Finish Line
+
+    # TODO uncomment the victoryDance() function call below (disabled for route testing efficiency)
+    # victoryDance(robot)
 
 ########## Run Program ##########
 cozmo.run_program(main)
