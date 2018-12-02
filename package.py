29c59d11f57b29311130e1fedfc8a8689b656720
Andrew Dillon <tajd1@unigroupinc.com>
2018-12-02 08:16:21 -0600

    Update package.py to zip everything up

--- package.py
+++ package.py
@@ -1,5 +1,8 @@
 #! /usr/bin/python
+import os
+from zipfile import ZipFile
 from subprocess import call, Popen, PIPE
+from shutil import rmtree
 
 cozmoclad_path_script = """
 import cozmoclad
@@ -8,6 +11,10 @@ print(cozmoclad.__path__[0])
 
 exit()
 """.encode()
+
+def clean():
+    rmtree('build')
+    rmtree('dist')
 
 def get_cozmoclad_path():
     process = Popen(['pipenv', 'run', 'python'], stdout=PIPE, stdin=PIPE)
@@ -23,4 +30,18 @@ def build_executable_file():
         'main.py'
     ])
 
+def zip_dir(zip, dir):
+    for root, dirs, files in os.walk(dir):
+        for file in files:
+            zip.write(os.path.join(root, file))
+
+def zip_all_the_things():
+    zip = ZipFile('cozmos_night_at_the_museum.zip', 'w', zipfile.ZIP_DEFLATED)
+    zip.write('windows_and_android.ps1')
+    zipdir(zip, 'platform-tools')
+    zipdir(zip, 'dist')
+    zip.close()
+
+clean()
 build_executable_file()
+zip_all_the_things()
--- run.ps1
+++
@@ -1,5 +0,0 @@
-$currentDirectory = (Get-Item -Path ".\").FullName
-$env:Path += ";$currentDirectory\platform-tools"
-"Loading..."
-./dist/main.exe
-Read-Host -Prompt "Press Enter to exit"
